4bba9f5273a11bd161a188c81ed876b37c4da93b
Louis-Philippe Gauthier <lpgauth@gmail.com>
2013-10-27 19:12:56 -0400

    Use binaries instead of strings

--- src/dlhttpc_lib.erl
+++ src/dlhttpc_lib.erl
@@ -34,10 +34,9 @@
         parse_url/1,
         format_request/7,
         header_value/2,
-        header_value/3,
-        normalize_method/1
+        header_value/3
     ]).
--export([maybe_atom_to_list/1]).
+-export([maybe_atom_to_binary/1]).
 
 -export([format_hdrs/1, dec/1]).
 
@@ -71,9 +70,9 @@ header_value(Hdr, Hdrs) ->
 header_value(Hdr, [{Hdr, Value} | _], _) ->
     Value;
 header_value(Hdr, [{ThisHdr, Value}| Hdrs], Default) ->
-    case string:equal(string:to_lower(ThisHdr), Hdr) of
-        true  -> Value;
-        false -> header_value(Hdr, Hdrs, Default)
+    case binary:match(ThisHdr, Hdr) of
+        nomatch -> header_value(Hdr, Hdrs, Default);
+        _Match  -> Value
     end;
 header_value(_, [], Default) ->
     Default.
@@ -85,11 +84,11 @@ header_value(_, [], Default) ->
 %% Will make any item, being an atom or a list, in to a list. If it is a
 %% list, it is simple returned.
 %% @end
--spec maybe_atom_to_list(atom() | list()) -> list().
-maybe_atom_to_list(Atom) when is_atom(Atom) ->
-    atom_to_list(Atom);
-maybe_atom_to_list(List) when is_list(List) ->
-    List.
+-spec maybe_atom_to_binary(atom() | binary()) -> binary().
+maybe_atom_to_binary(Atom) when is_atom(Atom) ->
+    atom_to_binary(Atom, latin1);
+maybe_atom_to_binary(Binary) when is_binary(Binary) ->
+    Binary.
 
 %% @spec (URL) -> {Host, Port, Path, Ssl}
 %%   URL = string()
@@ -102,38 +101,41 @@ maybe_atom_to_list(List) when is_list(List) ->
 parse_url(URL) ->
     % XXX This should be possible to do with the re module?
     {Scheme, HostPortPath} = split_scheme(URL),
-    {Host, PortPath} = split_host(HostPortPath, []),
-    {Port, Path} = split_port(Scheme, PortPath, []),
-    {string:to_lower(Host), Port, Path, Scheme =:= https}.
+    {Host, PortPath} = split_host(HostPortPath),
+    {Port, Path} = split_port(Scheme, PortPath),
+    {Host, Port, Path, Scheme =:= https}.
 
-split_scheme("http://" ++ HostPortPath) ->
+split_scheme(<<"http://", HostPortPath/binary>>) ->
     {http, HostPortPath};
-split_scheme("https://" ++ HostPortPath) ->
+split_scheme(<<"https://", HostPortPath/binary>>) ->
     {https, HostPortPath}.
 
-split_host([$: | PortPath], Host) ->
-    {lists:reverse(Host), PortPath};
-split_host([$/ | _] = PortPath, Host) ->
-    {lists:reverse(Host), PortPath};
-split_host([H | T], Host) ->
-    split_host(T, [H | Host]);
-split_host([], Host) ->
-    {lists:reverse(Host), []}.
+split_host(HostPortPath) ->
+    PortPath2 =
+        case binary:split(HostPortPath, <<":">>, [trim]) of
+            [HostPath] ->
+                case binary:split(HostPath, <<"/">>, [trim]) of
+                    [Host] ->
+                        <<"/">>;
+                    [Host | Path] ->
+                        <<"/", Path/binary>>
+                end;
+            [Host, PortPath] ->
+                PortPath
+        end,
+    {Host, PortPath2}.
 
-split_port(http, [$/ | _] = Path, []) ->
+split_port(http, <<"/", _Path/binary>> = Path) ->
     {80, Path};
-split_port(https, [$/ | _] = Path, []) ->
+split_port(https, <<"/", _Path/binary>> = Path) ->
     {443, Path};
-split_port(http, [], []) ->
-    {80, "/"};
-split_port(https, [], []) ->
-    {443, "/"};
-split_port(_, [], Port) ->
-    {list_to_integer(lists:reverse(Port)), "/"};
-split_port(_,[$/ | _] = Path, Port) ->
-    {list_to_integer(lists:reverse(Port)), Path};
-split_port(Scheme, [P | T], Port) ->
-    split_port(Scheme, T, [P | Port]).
+split_port(_Scheme, PortPath) ->
+    [Port | Path] = binary:split(PortPath, <<"/">>, [trim]),
+    Path2 = case Path of
+        [] -> <<"/">>;
+        [Path3] -> <<"/", Path3/binary>>
+    end,
+    {binary_to_integer(Port), Path2}.
 
 %% @spec (Path, Method, Headers, Host, Port, Body, PartialUpload) -> Request
 %% Path = iolist()
@@ -148,27 +150,12 @@ split_port(Scheme, [P | T], Port) ->
 format_request(Path, Method, Hdrs, Host, Port, Body, PartialUpload) ->
     AllHdrs = add_mandatory_hdrs(Method, Hdrs, Host, Port, Body, PartialUpload),
     IsChunked = is_chunked(AllHdrs),
-    {
-        IsChunked,
-        [
-            Method, " ", Path, " HTTP/1.1\r\n",
-            format_hdrs(AllHdrs),
-            format_body(Body, IsChunked)
-        ]
-    }.
-
-%% @spec normalize_method(AtomOrString) -> Method
-%%   AtomOrString = atom() | string()
-%%   Method = string()
-%% @doc
-%% Turns the method in to a string suitable for inclusion in a HTTP request
-%% line.
-%% @end
--spec normalize_method(atom() | string()) -> string().
-normalize_method(Method) when is_atom(Method) ->
-    string:to_upper(atom_to_list(Method));
-normalize_method(Method) ->
-    Method.
+    Request = [
+        <<Method/binary, " ", Path/binary, " HTTP/1.1\r\n">>,
+        format_hdrs(AllHdrs),
+        format_body(Body, IsChunked)
+    ],
+    {IsChunked, Request}.
 
 -spec format_hdrs(headers()) -> iolist().
 format_hdrs(Headers) ->
@@ -176,11 +163,11 @@ format_hdrs(Headers) ->
 
 format_hdrs([{Hdr, Value} | T], Acc) ->
     NewAcc = [
-        maybe_atom_to_list(Hdr), ": ", maybe_atom_to_list(Value), "\r\n" | Acc
+        <<Hdr/binary, ": ", (maybe_integer_to_binary(Value))/binary, "\r\n">> | Acc
     ],
     format_hdrs(T, NewAcc);
 format_hdrs([], Acc) ->
-    [Acc, "\r\n"].
+    [Acc, <<"\r\n">>].
 
 format_body(Body, false) ->
     Body;
@@ -199,29 +186,29 @@ add_mandatory_hdrs(Method, Hdrs, Host, Port, Body, PartialUpload) ->
     ContentHdrs = add_content_headers(Method, Hdrs, Body, PartialUpload),
     add_host(ContentHdrs, Host, Port).
 
-add_content_headers("POST", Hdrs, Body, PartialUpload) ->
+add_content_headers(<<"POST">>, Hdrs, Body, PartialUpload) ->
     add_content_headers(Hdrs, Body, PartialUpload);
-add_content_headers("PUT", Hdrs, Body, PartialUpload) ->
+add_content_headers(<<"PUT">>, Hdrs, Body, PartialUpload) ->
     add_content_headers(Hdrs, Body, PartialUpload);
 add_content_headers(_, Hdrs, _, _PartialUpload) ->
     Hdrs.
 
 add_content_headers(Hdrs, Body, false) ->
-    case header_value("content-length", Hdrs) of
+    case header_value(<<"Content-Length">>, Hdrs) of
         undefined ->
-            ContentLength = integer_to_list(iolist_size(Body)),
-            [{"Content-Length", ContentLength} | Hdrs];
+            ContentLength = iolist_size(Body),
+            [{<<"Content-Length">>, ContentLength} | Hdrs];
         _ -> % We have a content length
             Hdrs
     end;
 add_content_headers(Hdrs, _Body, true) ->
-    case {header_value("content-length", Hdrs),
-         header_value("transfer-encoding", Hdrs)} of
+    case {header_value(<<"Content-Length">>, Hdrs),
+         header_value(<<"Transfer-Encoding">>, Hdrs)} of
         {undefined, undefined} ->
-            [{"Transfer-Encoding", "chunked"} | Hdrs];
+            [{<<"Transfer-Encoding">>, <<"chunked">>} | Hdrs];
         {undefined, TransferEncoding} ->
             case string:to_lower(TransferEncoding) of
-            "chunked" -> Hdrs;
+            <<"chunked">> -> Hdrs;
             _ -> erlang:error({error, unsupported_transfer_encoding})
             end;
         {_Length, undefined} ->
@@ -231,18 +218,18 @@ add_content_headers(Hdrs, _Body, true) ->
     end.
 
 add_host(Hdrs, Host, Port) ->
-    case header_value("host", Hdrs) of
+    case header_value(<<"Host">>, Hdrs) of
         undefined ->
-            [{"Host", host(Host, Port) } | Hdrs];
+            [{<<"Host">>, host(Host, Port) } | Hdrs];
         _ -> % We have a host
             Hdrs
     end.
 
 is_chunked(Hdrs) ->
     TransferEncoding = string:to_lower(
-        header_value("transfer-encoding", Hdrs, "undefined")),
+        header_value(<<"Transfer-Encoding">>, Hdrs, "undefined")),
     case TransferEncoding of
-        "chunked" -> true;
+        <<"chunked">> -> true;
         _ -> false
     end.
 
@@ -251,4 +238,9 @@ dec(Num) when is_integer(Num) -> Num - 1;
 dec(Else)                     -> Else.
 
 host(Host, 80)   -> Host;
-host(Host, Port) -> [Host, $:, integer_to_list(Port)].
+host(Host, Port) -> <<Host/binary, $:, (integer_to_binary(Port))/binary>>.
+
+maybe_integer_to_binary(Integer) when is_integer(Integer) ->
+    integer_to_binary(Integer);
+maybe_integer_to_binary(Binary) when is_binary(Binary) ->
+    Binary.
