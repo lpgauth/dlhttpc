839dd6b37b8ad945ada8270042ba2c6cc0702a13
Louis-Philippe Gauthier <lpgauth@gmail.com>
2013-11-03 17:13:28 -0500

    Remove debug io:format/1

--- src/dlhttpc_lib.erl
+++ src/dlhttpc_lib.erl
@@ -116,7 +116,6 @@ split_host(HostPortPath) ->
     PortPath2 =
         case binary:split(HostPortPath, <<":">>, [trim]) of
             [HostPath] ->
-                io:format("hostpath: ~p~n", [HostPath]),
                 case binary:split(HostPath, <<"/">>, [trim]) of
                     [Host] ->
                         <<"/">>;
