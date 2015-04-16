e56267617d012d64d30adfeb1377c5b289a28635
Louis-Philippe Gauthier <lpgauth@gmail.com>
2015-04-16 11:33:16 -0400

    HACK: To make chunked response with keep-alive work...

--- src/dlhttpc_client.erl
+++ src/dlhttpc_client.erl
@@ -375,7 +375,7 @@ read_partial_body(State, _Vsn, Hdrs, chunked) ->
     Window = State#client_state.download_window,
     read_partial_chunked_body(State, Hdrs, Window, 0, [], 0);
 read_partial_body(State, Vsn, Hdrs, infinite) ->
-    check_infinite_response(Vsn, Hdrs),
+    % check_infinite_response(Vsn, Hdrs),
     read_partial_infinite_body(State, Hdrs, State#client_state.download_window);
 read_partial_body(State, _Vsn, Hdrs, {fixed_length, ContentLength}) ->
     read_partial_finite_body(State, Hdrs, ContentLength,
